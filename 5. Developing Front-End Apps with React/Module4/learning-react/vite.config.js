2758f0538c00c8ccd7a8b2cc79fe11802eac6c65
Jo3-h <joe.hosking1@gmail.com>
2024-09-27 19:02:32 +0930

    changed path name

--- 5. Developing Front-End Apps with React/Module4/learning-react/vite.config.js
+++ 5. Developing Front-End Apps with React/Module4/learning-react/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/5. Developing Front-End Apps with React/Module4/learning-react",
+  base: "/5.%20Developing%20Front-End%20Apps%20with%20React/Module4/learning-react",
   plugins: [react()],
 })
