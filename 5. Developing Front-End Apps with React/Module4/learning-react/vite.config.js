7fb0f8d4b54ad0b44e36fdb646e7b6afa6261c96
Jo3-h <joe.hosking1@gmail.com>
2024-09-27 18:35:09 +0930

    update gitignore file to all for node_modules to be seen in project folder

--- 5. Developing Front-End Apps with React/Module4/learning-react/vite.config.js
+++ 5. Developing Front-End Apps with React/Module4/learning-react/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/5. Developing Front-End Apps with React/Module4/learning-react",
   plugins: [react()],
 })
